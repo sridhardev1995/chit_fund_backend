9b3b2e9ed4b292188bb7d2fb1d0101d1d798bbf1
sridharzenfuture <sridharsb.zenfuture@gmail.com>
2026-07-24 17:37:58 +0530

    Fix postgres enum migration

--- database/migrations/2026_07_19_045839_add_partial_status_to_installments_table.php
+++ database/migrations/2026_07_19_045839_add_partial_status_to_installments_table.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    public function up(): void
+   public function up(): void
 {
     DB::statement("
         CREATE TYPE installment_status AS ENUM (
@@ -15,10 +15,37 @@ return new class extends Migration
         )
     ");
 
+    // Remove old default first
+    DB::statement("
+        ALTER TABLE installments
+        ALTER COLUMN status DROP DEFAULT
+    ");
+
+    // Change column type
     DB::statement("
         ALTER TABLE installments
         ALTER COLUMN status TYPE installment_status
         USING status::text::installment_status
+    ");
+
+    // Add default again
+    DB::statement("
+        ALTER TABLE installments
+        ALTER COLUMN status SET DEFAULT 'PENDING'
+    ");
+}
+
+
+public function down(): void
+{
+    DB::statement("
+        ALTER TABLE installments
+        ALTER COLUMN status TYPE VARCHAR(50)
+        USING status::text
+    ");
+
+    DB::statement("
+        DROP TYPE installment_status
     ");
 }
 };
